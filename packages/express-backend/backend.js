6561040cba8bf5cc6df885593d716c1c2a59fdb0
ryanvuu <ryanvu077@gmail.com>
2024-10-07 22:49:36 -0700

    Add GET request for specific users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -15,6 +15,23 @@ app.listen(port, () => {
   );
 });
 
+const findUserByName = (name) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name
+  );
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
 const users = {
   users_list: [
     {
@@ -44,7 +61,3 @@ const users = {
     }
   ]
 };
-
-app.get("/users", (req, res) => {
-  res.send(users);
-});
