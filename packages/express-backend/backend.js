b152d75331d7db6788c29eeb66789b08b71b2a7a
ryanvuu <ryanvu077@gmail.com>
2024-10-15 14:16:37 -0700

    Update post success code to 201

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -7,6 +7,36 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor"
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer"
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor"
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress"
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender"
+    }
+  ]
+};
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -25,6 +55,18 @@ const findUserByName = (name) => {
 
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
+
+const findUserByJob = (job) => {
+  return users["users_list"].filter(
+    (user) => user["job"] === job
+  )
+};
+
+const findUserByNameJob = (name, job) => {
+  return users["users_list"].filter(
+    (user) => user["name"] === name && user["job"] === job
+  );
+};
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
@@ -49,18 +91,6 @@ app.delete("/users/:id", (req, res) => {
   removeUser(id);
   res.send();
 });
-
-const findUserByJob = (job) => {
-  return users["users_list"].filter(
-    (user) => user["job"] === job
-  )
-};
-
-const findUserByNameJob = (name, job) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name && user["job"] === job
-  );
-};
 
 app.get("/users", (req, res) => {
   const name = req.query.name;
@@ -92,36 +122,11 @@ const addUser = (user) => {
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.send();
+  addUser(userToAdd)
+  let result = (findUserByName(userToAdd))
+  if (result == undefined) {
+    res.status(400).send("Bad Request")
+  } else {
+    res.status(201).send("Lol");
+  }
 });
-
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
